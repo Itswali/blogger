eab18cef780dd8cf76efc4c5e321ee7a646062de
Itswali <luckywali0986@gmail.com>
2023-06-08 00:16:54 +0500

    fix update increment error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_create :update_comments_counter
-  after_destroy :update_comments_counter
+  after_create :increment_comments_counter
+  after_destroy :increment_comments_counter
 
   private
 
   def increment_comments_counter
-    post.update_comments_counter
+    post.increment_comments_counter
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,12 +2,12 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_create :update_likes_counter
-  after_destroy :update_likes_counter
+  after_create :increment_likes_counter
+  after_destroy :increment_likes_counter
 
   private
 
-  def update_likes_counter
-    post.update_likes_counter
+  def increment_likes_counter
+    post.increment_likes_counter
   end
 end
