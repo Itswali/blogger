ced065e32240dfabf9ffc9cb9f200fa54544ba09
Itswali <luckywali0986@gmail.com>
2023-06-07 22:42:25 +0500

    add models and data and queries for models

---
+++ app/models/comment.rb
@@ -0,0 +1,14 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
+  private
+
+  def increment_comments_counter
+    post.update_comments_counter
+  end
+
+end
